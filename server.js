71a0b1a558fd7c5ffa6aef68abd357d997fb156d
Jaya12563 <jayakandukuri1@gmail.com>
2025-12-14 22:40:55 +0530

    server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const auth = require("./auth");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/api/auth/signup", auth.signup);
+app.post("/api/auth/login", auth.login);
+app.post("/api/auth/google", auth.googleLogin);
+
+app.listen(5000, () => {
+  console.log("🚀 Backend running at http://localhost:5000");
+});
 
